faa2b0004728d075a270128a5ee741773aebfc7f
Laurant Marijnissen <laurant@gigafyde.dev>
2021-01-17 18:33:00 +0100

    Initial commit of a working bot

---
+++ src/main/java/dev/gigafyde/autobatreact/Main.java
@@ -0,0 +1,20 @@
+package dev.gigafyde.autobatreact;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static JDA jda;
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+        jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                .addEventListeners(new MessageListener())
+                .build();
+        jda.awaitReady();
+        MessageListener.batemote = jda.getEmoteById("797601839717023744");
+        log.info("Bat bot activated");
+    }
+}
---
+++ src/main/java/dev/gigafyde/autobatreact/MessageListener.java
@@ -0,0 +1,14 @@
+package dev.gigafyde.autobatreact;
+
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter {
+    public static Emote batemote;
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        event.getMessage().addReaction(batemote).queue();
+    }
+}
